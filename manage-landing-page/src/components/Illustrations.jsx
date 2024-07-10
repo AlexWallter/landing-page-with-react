3eb7125159ee02a846680d09cb623e665e5491a3
Alex Walter <alexwalter129@gmail.com>
2024-07-10 19:49:21 -0300

    routes

--- manage-landing-page/src/components/Illustrations.jsx
+++ manage-landing-page/src/components/Illustrations.jsx
@@ -2,7 +2,7 @@ export default function Illustrations() {
     return (
         <>
             <div className="img-container main-illustration">
-                <img src="/landing-page-with-react/assets/illustrations/illustration-intro.svg" alt="graph illustration" />
+                <img src="/landing-page-with-react/assets/illustration-intro.svg" alt="graph illustration" />
             </div>
         </>
     )
